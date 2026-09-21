a63e36a870cf1a764cd03b3e20cb2339b42c2972
5ku11cru5h3r <th3.yash.jha@proton.me>
2026-09-21 23:45:43 +0530

    Refactor App.java: Add simple_interest method and update prop_of_circle method; replace area_of_circle with prop_of_circle in main

--- src/App.java
+++ src/App.java
@@ -1,22 +1,35 @@
 import java.util.Scanner;
 
 public class App {
+    public static void simple_interest() {
+        try (Scanner sc = new Scanner(System.in)) {
+            
+        }
 
-    public static void area_of_circle() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter r:");
-        double r = sc.nextDouble();
-        double area_of_circle = Math.PI * r * r;
-        System.out.println(area_of_circle);
+    }
+
+    public static void prop_of_circle() {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter r:");
+            double r = sc.nextDouble();
+            double area_of_circle = Math.PI * r * r;
+            double circumference = 2 * area_of_circle / r;
+            System.out.println("Area ==>");
+            System.out.printf("%.2f\n", area_of_circle);
+            System.out.println("Circumference ==>");
+            System.out.printf("%.2f\n", circumference);
+            // System.out.println(circumference);
+        }
     }
 
     public static void add_two_numbers() throws Exception {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a:");
-        double a = sc.nextDouble();
-        System.out.println("Enter b:");
-        double b = sc.nextDouble();
-        System.out.println("Result: \n {a+b}:" + (a + b));
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter a:");
+            double a = sc.nextDouble();
+            System.out.println("Enter b:");
+            double b = sc.nextDouble();
+            System.out.println("Result: \n {a+b}:" + (a + b));
+        }
     }
 
     public static void HelloWorld() {
@@ -25,6 +38,6 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         // add_two_numbers();
-        area_of_circle();
+        prop_of_circle();
     }
 }
